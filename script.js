1bd179bfb581f0ec2b5e276429554788d2baf5cd
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2022-06-23 13:31:22 +0100

    update

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const aboutme = {
+const about_me ={
     name: 'Oladeni ifeoluwa',
     height: '5.9 inches',
     country: 'Nigeria',
-};
+}
 
 alert(
-    `My name is ${aboutme.name} , i am ${aboutme.height} tall and i  am from ${aboutme.country}.`
+    `My name is ${about_me.name} , i am ${aboutme_height} tall and i  am from ${about_me.country}.`
 );
